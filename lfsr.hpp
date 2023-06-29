8446a675220c8b4eeea729955ee447a88d363dc7
Anatoly V Novikov <46084344+nawww83@users.noreply.github.com>
2023-06-29 22:18:13 +0300

    Add files via upload
    Add LFSR (Linear Feedback Shift Register) class and some functions (see main.cpp) to find the coefficients of the polynomial giving the largest period T = p^m - 1, where p - a prime number, m - the register length.

---
+++ lfsr.hpp
@@ -0,0 +1,64 @@
+#include <cstdint>
+#include <cassert>
+#include <array>
+
+
+namespace lfsr8 {
+using u32 = uint32_t;
+using u32x8 = std::array<u32, 8>;
+
+
+static auto is_zero(u32x8 state) {
+	bool zf = true;
+	for (int i=0; i<8; ++i) {
+		zf &= (state[i] == 0);
+	}
+	return zf;
+}
+
+template <int p, int m>
+class LFSR {
+    static_assert(m <= 8);
+	static_assert(m > 0);
+	static_assert(p < 256);
+	static_assert(p > 1);
+public:
+    constexpr LFSR(u32x8 K): m_K(K) {};
+	void set_state(u32x8 state) {
+		m_state = state;
+		v = m_state[m-1];
+	}
+    void set_unit_state() {
+		m_state = {1};
+		v = m_state[m-1];
+	}
+	void set_K(u32x8 K) {
+		m_K = K;
+	}
+	void next(u32 input=0) {
+        for (int i=m-1; i>0; i--) {
+            m_state[i] = (m_state[i-1] + v*m_K[i]) % p;
+		}
+        m_state[0] = (input + v*m_K[0]) % p;
+        v = m_state[m-1];
+	}
+	void saturate(int q=m) {
+		assert(! is_zero(m_state));
+		assert(q >= m);
+		for (int i=0; i<q; ++i) {
+			next();
+		}
+	}
+	bool is_state(u32x8 state) const {
+		return (state == m_state);
+	}
+	auto get_state() const {
+		return m_state;
+	}
+private:
+	u32x8 m_state {};
+	u32x8 m_K {};
+	u32 v {};
+};
+
+}
---
+++ main.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <random>
+#include <cmath>
+#include <cstring>
+#include "lfsr.hpp"
+
+
+constexpr int p = 131;
+constexpr int m = 4;
+
+using LFSR = lfsr8::LFSR<p, m>;
+using STATE = lfsr8::u32x8;
+
+template<int x>
+constexpr auto is_prime() {
+	int d = 2;
+	bool ok = true;
+	while (d <= int(std::sqrt(x))) {
+		ok &= ((x % d) != 0);
+		d += 1;
+	}
+	return ok;
+}
+
+auto add_separators(std::string& value, char thousandSep = '\'') {
+    int len = value.length();
+    int dlen = 3;
+    while (len > dlen) {
+        value.insert(len - dlen, 1, thousandSep);
+        dlen += 4;
+        len += 1;
+    }
+    return value;
+}
+
+auto calculate_period(LFSR& g) {
+	long long T = 1;
+	// const auto old_state = g.get_state();
+	g.set_unit_state();
+	g.saturate();
+	const auto ref = g.get_state();
+	while (true) {
+		g.next();
+		if (g.is_state(ref))
+			break;
+		T += 1;
+	}
+	// g.set_state(old_state);
+	return T;
+}
+
+auto find_max_period_polynomial(long long& T) {
+	STATE K = {1};
+	LFSR g(K);
+	
+	std::random_device rd;
+    std::mt19937 gen(rd());
+    std::geometric_distribution<> d(0.3);
+	T = 0;
+	const long long T_ref = std::pow(p, m) - 1;
+	while (T != T_ref) {
+		for (int i=0; i<m; ++i) {
+			K[i] = d(gen) % p;
+		}
+		g.set_K(K);
+		T = calculate_period(g);
+	}
+	return K;
+}
+
+
+int main() {
+	using namespace std;
+	
+	static_assert(is_prime<p>());
+	
+	cout << "LFSR with modulo p: " << p << ", length m: " << m << endl;
+	
+	cout << "Wait for max period T = p^m - 1 polynomial look up..." << endl;
+	long long T;
+	STATE K = find_max_period_polynomial(T);
+	cout << "Found coefficients K: (";
+	for (int i=0; i<m-1; ++i) {
+		cout << K[i] << ", ";
+	}
+	cout << K[m-1] << ")" << endl;
+	auto T_str = std::to_string(T);
+	cout << "Period T: " << add_separators(T_str) << endl;
+
+    return 0;
+}
