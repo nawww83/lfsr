6327bee2843683d07ebeaf02c8190d917862e70d
Anatoly V Novikov <46084344+nawww83@users.noreply.github.com>
2023-06-29 22:29:16 +0300

    Update lfsr.hpp

--- lfsr.hpp
+++ lfsr.hpp
@@ -2,11 +2,9 @@
 #include <cassert>
 #include <array>
 
-
 namespace lfsr8 {
 using u32 = uint32_t;
 using u32x8 = std::array<u32, 8>;
-
 
 static auto is_zero(u32x8 state) {
 	bool zf = true;
@@ -18,17 +16,17 @@ static auto is_zero(u32x8 state) {
 
 template <int p, int m>
 class LFSR {
-    static_assert(m <= 8);
+	static_assert(m <= 8);
 	static_assert(m > 0);
 	static_assert(p < 256);
 	static_assert(p > 1);
 public:
-    constexpr LFSR(u32x8 K): m_K(K) {};
+    	constexpr LFSR(u32x8 K): m_K(K) {};
 	void set_state(u32x8 state) {
 		m_state = state;
 		v = m_state[m-1];
 	}
-    void set_unit_state() {
+   	void set_unit_state() {
 		m_state = {1};
 		v = m_state[m-1];
 	}
@@ -36,11 +34,11 @@ public:
 		m_K = K;
 	}
 	void next(u32 input=0) {
-        for (int i=m-1; i>0; i--) {
-            m_state[i] = (m_state[i-1] + v*m_K[i]) % p;
+	    for (int i=m-1; i>0; i--) {
+	        m_state[i] = (m_state[i-1] + v*m_K[i]) % p;
 		}
-        m_state[0] = (input + v*m_K[0]) % p;
-        v = m_state[m-1];
+		m_state[0] = (input + v*m_K[0]) % p;
+		v = m_state[m-1];
 	}
 	void saturate(int q=m) {
 		assert(! is_zero(m_state));
